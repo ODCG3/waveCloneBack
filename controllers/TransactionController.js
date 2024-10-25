d4bde1d6872b44319725783fbe2d42620a45089d
bakemono-san <mousdef34@gmail.com>
2024-10-25 18:50:44 +0000

    feat(depot/retrait) "implementing depot and retrait"
    closes #39

--- controllers/TransactionController.js
+++ controllers/TransactionController.js
@@ -93,6 +93,112 @@ class TransactionController {
             await this.repository.prisma.$disconnect();
         }
     }
+
+    static async depot(req,res){
+        let user = await this.repository.prisma.users.findUnique({
+            where: { id: req.user.userId },
+        });
+
+        const code = req.body.code;
+
+        if(user.code != code){
+            return res.status(401).json({ message: "Vous n'êtes pas autorisé à effectuer cette action" });
+        }
+
+        const client = req.body.numeroClient;
+        client = await this.repository.prisma.users.findFirst({ where: {
+            telephone : numeroClient
+        } });
+
+        if(!client){
+            return res.status(400).json({ message: "Le numero est erronée" });
+        }
+
+        const montant = req.body.montant;
+
+        if(!montant || montant <= 500){
+            return res.status(400).json({ message: "Le montant est obligatoire et doit etre supperieur a 500" });
+        }
+
+        const data = this.repository.prisma.users.update({
+            where: { id: client.id },
+            data: { solde: parseFloat(client.solde) + parseFloat(montant) },
+        });
+
+        this.repository.prisma.transaction.create({
+            data: {
+                montant: parseFloat(montant),
+                users_transaction_agentTousers: {
+                    connect: { id: user.id }, // Connects `exp` relation (if the same user)
+                },
+                users_transaction_destinataireTousers: {
+                    connect: { id: client.id }, // Connects `destinataire` relation
+                },
+                type: {
+                    connect: { id: 2 }, // Connects type relation
+                },
+            },
+        })
+
+        const message = data ? "Depot effectué avec succés" : "Erreur lors de la transaction";
+        const status = data ? 200 : 400;
+
+        return res.status(status).json(instance.formatResponse(data, status, message, null));
+    }
+
+    static async retrait(req, res){
+        let user = await this.repository.prisma.users.findUnique({
+            where: { id: req.user.userId },
+        });
+
+        const code = req.body.code;
+
+        if(user.code!= code){
+            return res.status(401).json({ message: "Vous n'êtes pas autorisé à effectuer cette action" });
+        }
+
+        const client = req.body.numeroClient;
+        client = await this.repository.prisma.users.findFirst({ where: {
+            telephone : numeroClient
+        } });
+
+        if(!client){
+            return res.status(400).json({ message: "Le numero est erronée" });
+        }
+
+        const montant = req.body.montant;
+
+        if(!montant || montant <= 500){
+            return res.status(400).json({ message: "Le montant est obligatoire et doit etre positif et supperieur a 499 francs" });
+        }
+
+        const retrait = await this.repository.prisma.users.update({ where: {id: client.id}, data: { solde: parseFloat(client.solde) - parseFloat(montant) } });
+
+        const transaction = await this.repository.prisma.transaction.create({
+            data: {
+                montant: parseFloat(montant),
+                users_transaction_agentTousers: {
+                    connect: { id: user.id }, // Connects `exp` relation (if the same user)
+                },
+                users_transaction_expTousers: {
+                    connect: { id: client.id }, // Connects `destinataire` relation
+                },
+                type: {
+                    connect: { id: 3 }, // Connects type relation
+                },
+            },
+        });
+
+        if(!transaction){
+            return res.status(400).json({ message: "Une erreur est survenue lors de la création de la transaction" });
+        }
+
+        const status = retrait ? 201 : 400;
+        const message = retrait? "Retrait effectué avec succés" : "Erreur lors de la transaction";
+        const data = retrait ? retrait : []
+
+        res.status(status).json(instance.formatResponse(data, message, status,null));
+    }
 }
 
 export default TransactionController;
