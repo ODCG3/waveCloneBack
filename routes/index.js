9107ad6f27d9724ac9a81981a6e366d8325bc0e1
ZahraThiaw <fatimatathiaw6@gmail.com>
2024-10-26 16:31:41 +0000

    Sauvegarde des modifications locales avant pull
    valider les modifications

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
 import UserController from '../controllers/UserController.js';
+
 import auth from '../middleware/auth.js';
+
+import PaymentController from '../controllers/PaymentController.js';
+import BillPaymentController from '../controllers/BillPaymentController.js';
+import MerchantPaymentController from '../controllers/MerchantPaymentController.js';
+import CardController from '../controllers/CardController.js';
+
+
 const router = express.Router();
 
 /* GET home page. */
@@ -20,8 +28,31 @@ router.route('/logout')
 router.route('/reinitializeCode')
 .post(auth,(req,res)=> UserController.reinitializeCode(req,res));
 
+//Issues endpoint
+router.route('/users/reportIssue')
+.post(auth, (req, res) => UserController.reportIssue(req, res));
+
+router.route('/admin/respondIssue')
+.post(auth, (req, res) => UserController.respondToIssueInApp(req, res));
+
+
 // Example endpoint
+
 router.get('/users', (req,res) => UserController.getAll(req,res));
+
+router.get('/example', (req,res) => UserController.getAll(req,res));
+
+// Endpoint pour effectuer un paiement marchand
+router.post('/merchant-payment', (req, res) => MerchantPaymentController.payMerchant(req, res));
+
+
+//endpoint pour le paiement d'un facture 
+router.post('/bill-payment', (req, res) => BillPaymentController.payBill(req, res));
+
+
+//endpoint pour verouiller la carte en cas de perte
+router.post('/card/lock', (req, res) => CardController.lockCard(req, res));
+
 
 // Export the router
 export default router;
